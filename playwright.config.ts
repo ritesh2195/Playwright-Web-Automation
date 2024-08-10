6e183d34394e0c0484983a1ac389450c53d41931
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2024-08-10 16:07:40 +0530

    browserstack implementation

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
      },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -50,7 +50,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
